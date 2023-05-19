ab2dfaa2c806119c91d7075706e937e1082ebe81
Seseg Vanchikova <seseg_vanchikova@epam.com>
2023-05-19 14:15:07 +0300

    not found

--- src/main/java/com/github/seseque/dockerusers/users/UserController.java
+++ src/main/java/com/github/seseque/dockerusers/users/UserController.java
@@ -1,6 +1,7 @@
 package com.github.seseque.dockerusers.users;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,10 +26,14 @@ public class UserController {
         return userService.createUser(user);
     }
 
-    //    404 - User doesn’t exist with given id
     @GetMapping("/users/{id}")
-    UserRes getUser(@PathVariable Long id) {
-        return userService.getUser(id);
+    ResponseEntity<UserRes> getUser(@PathVariable Long id) {
+        try {
+            UserRes res = userService.getUser(id);
+            return ResponseEntity.ok(res);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/users/{id}")
